c6c71ccdda25fb1a2e9837e03117c55cfc8ba28a
rexkk1212 <160700808+rexkk1212@users.noreply.github.com>
2024-03-27 14:49:36 +0800

    111

---
+++ Las Vegas/app-files/data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-the-welcome-to-las-vegas-sign",
+      "name": "The Welcome to Las Vegas Sign",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.16689968090240548,
+        "pitch": -0.35408651460455154,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10572274159023465,
+          "pitch": 0.6239808922148153,
+          "rotation": 0,
+          "target": "1-new-york-new-york-hotel-and-casino"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7638657543921958,
+          "pitch": -0.5611981245733269,
+          "title": "The Welcome to Las Vegas Sign",
+          "text": "The Welcome to Las Vegas Sign , like most of the Strip, sits in Paradise and is located roughly 4 miles (6.4 km) south of the actual city limits of Las Vegas, refering to the entire metro area as \"Las Vegas\"."
+        }
+      ]
+    },
+    {
+      "id": "1-new-york-new-york-hotel-and-casino",
+      "name": "New York-New York Hotel and Casino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.2353808003473414,
+        "pitch": -0.2756816435135505,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12760572885140853,
+          "pitch": 0.2140143088046873,
+          "rotation": 0,
+          "target": "0-the-welcome-to-las-vegas-sign"
+        },
+        {
+          "yaw": -3.098774877706971,
+          "pitch": 0.14526913813130093,
+          "rotation": 0,
+          "target": "2-planet-hollywood-las-vegas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2353808003473414,
+          "pitch": -0.2756816435135505,
+          "title": "New York-New York Hotel and Casino",
+          "text": "New York-New York Hotel and Casino is a casino hotel on the Las Vegas Strip in Paradise, Nevada, United States. It is owned by Vici Properties and operated by MGM Resorts International, and is designed to evoke New York City in its architecture and other aspects. The design features downsized replicas of numerous city landmarks such as the Statue of Liberty."
+        }
+      ]
+    },
+    {
+      "id": "2-planet-hollywood-las-vegas",
+      "name": "Planet Hollywood Las Vegas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 2.979093212931059,
+        "pitch": -0.042996219630557775,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3782486728500416,
+          "pitch": 0.3695956540960612,
+          "rotation": 0,
+          "target": "3-paris-las-vegas"
+        },
+        {
+          "yaw": -1.538521135592399,
+          "pitch": 0.40169567199855827,
+          "rotation": 0,
+          "target": "1-new-york-new-york-hotel-and-casino"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.913111487670367,
+          "pitch": 0.0797117017474509,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Planet Hollywood Las Vegas</span>",
+          "text": "Planet Hollywood Las Vegas (formerly the Aladdin) is a casino hotel on the Las Vegas Strip in Paradise, Nevada, United States. It is owned and operated by Caesars Entertainment. The resort includes a 64,470 sq ft (5,989 m2) casino and 2,494 hotel rooms. It also features the Miracle Mile Shops and a 7,000-seat entertainment venue known as Bakkt Theater."
+        }
+      ]
+    },
+    {
+      "id": "3-paris-las-vegas",
+      "name": "Paris Las Vegas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.1351383662562613,
+        "pitch": -0.5805598306896051,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05406691968915034,
+          "pitch": 0.30060925750326106,
+          "rotation": 0,
+          "target": "4-bellagio-"
+        },
+        {
+          "yaw": -3.106428242258522,
+          "pitch": 0.24298067078831664,
+          "rotation": 0,
+          "target": "2-planet-hollywood-las-vegas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0478523972067517,
+          "pitch": -0.4472845296222321,
+          "title": "Paris Las Vegas",
+          "text": "Paris Las Vegas is a casino hotel on the Las Vegas Strip in Paradise, Nevada. It is owned and operated by Caesars Entertainment. Property features include a 95,263-square-foot (8,850.2 m2) casino, 2,916 hotel rooms, a 1,400-seat performance theater, and various restaurants. The Paris-themed resort also includes a half scale replica of the Eiffel Tower, rising 540 feet (164.6 m). Replicas of other Paris landmarks are featured as well, including the Arc de Triomphe, the Louvre, the Paris Opera House, and the Musée d'Orsay."
+        }
+      ]
+    },
+    {
+      "id": "4-bellagio-",
+      "name": "Bellagio ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.603684672556275,
+          "pitch": 0.33724364495713033,
+          "rotation": 0,
+          "target": "5-casino-royal"
+        },
+        {
+          "yaw": -2.0712327172026797,
+          "pitch": 0.27428831252912644,
+          "rotation": 0,
+          "target": "3-paris-las-vegas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3081069416467912,
+          "pitch": -0.3583425733437675,
+          "title": "Bellagio",
+          "text": "Bellagio&nbsp; is a resort, luxury hotel and casino on the Las Vegas Strip in Paradise, Nevada. It is owned by Blackstone Inc. and operated by MGM Resorts International. Bellagio was conceived by casino owner Steve Wynn, and was built on the former site of the Dunes hotel-casino. Wynn's company, Mirage Resorts, purchased the Dunes in 1992. Plans were announced in 1994 to replace it with Beau Rivage, a French-themed resort. However, Wynn changed the project plans in 1995, instead theming it after the village of Bellagio, near Lake Como. The resort was designed by Jon Jerde. Construction began on November 1, 1995, with Marnell Corrao Associates as general contractor."
+        }
+      ]
+    },
+    {
+      "id": "5-casino-royal",
+      "name": "Casino Royal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1015940566349833,
+          "pitch": 0.2392701236403063,
+          "rotation": 0,
+          "target": "4-bellagio-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.633967906654946,
+          "pitch": -0.0668082215703194,
+          "title": "Casino Royale",
+          "text": "The Best Western Plus Casino Royale (formerly known as the Nob Hill Casino and Casino Royale) is a casino and hotel on the Las Vegas Strip in Paradise, Nevada. It is owned and operated by Tom Elardi. The casino, measuring 19,000 sq ft (1,800 m2), caters to low rollers."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ home/app-files/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-welcome-to-las-vegas",
+      "name": "welcome to las vegas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.11876099890937653,
+        "pitch": -0.4079949941611236,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
